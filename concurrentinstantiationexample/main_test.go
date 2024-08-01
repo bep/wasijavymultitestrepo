ac26245afdeb39a7a49edaaf95467fd77c6d9d62
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2024-08-01 09:16:27 +0200

    Add the concurrent-instantiation from wazero repo
    ```
    BenchmarkConcurrentInstantiation/OneRuntime/goroutines=10-10                5245            236092 ns/op          497181 B/op        528 allocs/op
    BenchmarkConcurrentInstantiation/OneRuntime/goroutines=50-10                2149            551476 ns/op         1258045 B/op       1654 allocs/op
    BenchmarkConcurrentInstantiation/OneRuntime/goroutines=100-10               1268            937423 ns/op         2208523 B/op       3059 allocs/op
    BenchmarkConcurrentInstantiation/OneRuntime/goroutines=300-10                435           3019852 ns/op         6011552 B/op       8691 allocs/op
    BenchmarkConcurrentInstantiation/ManyRuntime/goroutines=10-10               2733            380479 ns/op          919463 B/op       1060 allocs/op
    BenchmarkConcurrentInstantiation/ManyRuntime/goroutines=50-10               1076           1124723 ns/op         2184652 B/op       3868 allocs/op
    BenchmarkConcurrentInstantiation/ManyRuntime/goroutines=100-10               483           2556008 ns/op         5219776 B/op       8078 allocs/op
    BenchmarkConcurrentInstantiation/ManyRuntime/goroutines=300-10               210           5813977 ns/op        11871761 B/op      22257 allocs/op
    ```

---
+++ concurrentinstantiationexample/main_test.go
@@ -0,0 +1,34 @@
+package concurrentinstantiationexample
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRunOneRuntime(t *testing.T) {
+	OneRuntime(50)
+}
+
+func TestRunOneRuntimePerInstance(t *testing.T) {
+	OneRuntimePerInstance(50)
+}
+
+func BenchmarkConcurrentInstantiation(b *testing.B) {
+	runBenchmark := func(b *testing.B, f func(int)) {
+		for _, i := range []int{10, 50, 100, 300} {
+			b.Run(fmt.Sprintf("goroutines=%d", i), func(b *testing.B) {
+				for j := 0; j < b.N; j++ {
+					f(i)
+				}
+			})
+		}
+	}
+
+	b.Run("OneRuntime", func(b *testing.B) {
+		runBenchmark(b, OneRuntime)
+	})
+
+	b.Run("ManyRuntime", func(b *testing.B) {
+		runBenchmark(b, OneRuntimePerInstance)
+	})
+}
